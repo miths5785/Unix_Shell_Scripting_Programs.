b9eb0eca0338fafbab7c37cc4cfc20ad19ce72b9
Mithilesh Bhutada <48923599+miths5785@users.noreply.github.com>
2019-04-23 23:51:55 +0530

    Update String.sh

--- String.sh
+++ String.sh
@@ -6,6 +6,6 @@ out=$x$y
 len=$(expr length $out)
 echo "Before Concatenation String 1: $x"
 echo "Before Concatenation String 2: $y"
-echo "After Concatenation Final String is: $out"
-echo "The length of the String is: $len"
+echo "After Concatenation Final String Is: $out"
+echo "The Length Of The String Is: $len"
 
